696333a5889cb4f68e19b13a3541644748959c22
Cesar64Garcia <cesar_garcia7497@hotmail.com>
2018-09-15 00:29:53 -0600

    handlers

--- react-demo-app/src/App.js
+++ react-demo-app/src/App.js
@@ -7,6 +7,20 @@ class App extends Component {
     age: 21
   }
 
+  handleClick = (e) => {
+    this.setState({
+      name: 'Cesar Garcia'
+    });
+  }
+
+  handleMouseOver = (e) => {
+    console.log(e.target, e.pageX);
+  }
+
+  handleCopy = (e) => {
+    console.log('Be original!!')
+  }
+
   render() {
     return (
       <div>
@@ -14,6 +28,11 @@ class App extends Component {
           <h1>Welcome to React. </h1>
           <p>My name is {this.state.name} and my age is {this.state.age}</p>
         </header>
+        <section>
+          <button onClick={this.handleClick}>Click Me!</button>
+          <button onMouseOver={this.handleMouseOver}>Hover me</button>
+          <p onCopy={this.handleCopy}>What we think, we become.</p>
+        </section>
       </div>
     );
   }
